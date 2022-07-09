c15d1b41fe122bc1ff07004ea2abfc5c89e9a0b0
hyowon <hyowon9964@gmail.com>
2022-07-10 02:30:53 +0900

    Add: 전체 캠핑장 페이지 path 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Main from "./pages/Main";
+import AllCampsite from "./pages/AllCampsite";
 
 function App() {
   return (
       <Routes>
         <Route exact path="/" element={<Main />}/>
+        <Route path="/campsites" element={<AllCampsite />}/>
       </Routes>
   )
 }
---
+++ src/pages/AllCampsite/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllCampsite = () => {
+  return (
+      <div>
+        전체 캠핑장 페이지
+      </div>
+  )
+}
+
+export default AllCampsite;
