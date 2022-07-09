2a1824972d0ee50f3ace84a037f2d693cfc70975
hyowon <hyowon9964@gmail.com>
2022-07-09 22:58:26 +0900

    Add: router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import Main from "./pages/Main";
 
 function App() {
-  return <div>CaT</div>
+  return (
+      <Routes>
+        <Route path="/" element={<Main />}/>
+      </Routes>
+  )
 }
 
 export default App;
