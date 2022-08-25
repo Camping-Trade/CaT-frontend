203b6466b5b83480838431fc6ae6fbc7591ea4f1
hyowonii <hyowon9964@gmail.com>
2022-08-25 19:01:06 +0900

    Design: 상세 캠핑장 페이지 UI

--- src/pages/ContentDetail/index.js
+++ src/pages/ContentDetail/index.js
@@ -8,19 +8,47 @@ import Footer from "../../components/Footer";
 import {PageWrapper} from "../../styles/PageLayout";
 import {
   BottomTitle,
-  BottomWrapper, ImgInputLabel, ImgWrapper,
+  BottomWrapper,
+  ImgInputLabel,
+  UploadImgWrapper,
   NewCommentContainer,
-  NewCommentWrapper, PreviewWrapper, ReviewCard, ReviewInfo, ReviewImgs, ReviewWrapper, ReviewWriter, StarWrapper,
+  NewCommentWrapper,
+  PreviewWrapper,
+  ReviewCard,
+  ReviewInfo,
+  ReviewImgs,
+  ReviewWrapper,
+  ReviewWriter,
+  StarWrapper,
   TextAreaWrapper,
   TopWrapper,
-  UploadBtn, ReviewContent
+  UploadBtn,
+  ReviewContent,
+  LeftWrapper,
+  Title,
+  LeftInfo,
+  Map,
+  RightWrapper,
+  ImgsWrapper,
+  DetailWrapper,
+  GrayDetail,
+  ShortComment, LongComment
 } from "./style";
 import {BiImageAdd} from "react-icons/bi";
+import {StyledAtag} from "../../styles/StyledLink";
+import Color from "../../styles/Color";
+import StyledBtn from "../../styles/StyledBtn";
+
 
 const ContentDetail = () => {
   const params = useParams();
   const location = useLocation();
-  // console.log(params, location)
+  console.log(params, location)
+
+  // 캠핑장 정보
+  const Campsite = location.state.data.campsite;
+  // console.log(Campsite);
+
 
   // 후기 입력 내용
   const [content, onChangeContent, setContent] = useInput("");
@@ -52,6 +80,11 @@ const ContentDetail = () => {
     })
   }
 
+  const [showLongComment, setShowLongComment] = useState(false);
+  const onClickLongComment = () => {
+    setShowLongComment(prev => !prev);
+  }
+
 
   // 리뷰 목록 show
   const ShowReviews = reviews.map((review, index) => {
@@ -78,11 +111,149 @@ const ContentDetail = () => {
       <div>
         <Header />
         <PageWrapper>
+
           {/* 상세 캠핑장 정보 */}
-          <TopWrapper>정보</TopWrapper>
+          <TopWrapper>
+            <LeftWrapper>
+              <Title>{Campsite.facltNm}</Title>
+              {/* 이미지 */}
+              <ImgsWrapper>
+                <StyledBtn>◀︎prev</StyledBtn>
+                <img src={Campsite.firstImageUrl} alt="대표이미지" />
+                <StyledBtn>next▶︎</StyledBtn>
+              </ImgsWrapper>
+
+              <LeftInfo>주소: {Campsite.addr1} {Campsite.addr2}</LeftInfo>
+              <LeftInfo>Tel: {Campsite.tel}</LeftInfo>
+              <StyledAtag
+                  color="gray"
+                  hover={Color.pointcolor}
+                  href={Campsite.homepage}>
+                홈페이지 바로가기
+              </StyledAtag>
+              <Map>지도</Map>
+              <div>{Campsite.direction}</div>
+            </LeftWrapper>
+
+            <RightWrapper>
+              {/* 캠핑장 디테일 정보 */}
+              <DetailWrapper>
+                <ShortComment>
+                  "{Campsite.lineIntro}"
+                  <StyledBtn onClick={onClickLongComment}>더보기</StyledBtn>
+                </ShortComment>
+                <LongComment show={showLongComment}>{Campsite.intro}</LongComment>
+                <GrayDetail>
+                  <br/>
+                  ☐ 업종
+                  <br />
+                  {Campsite.induty}
+                  <br />
+                  ☐ 입지 구분
+                  <br />
+                  {Campsite.lctCl}
+                  <br/>
+                  ☐ 테마 환경
+                  <br/>
+                  {Campsite.themaEnvrnCl}
+                  <br/><br/>
+
+                  [운영 안내]
+                  <br/>
+                  {Campsite.operPdCl} / {Campsite.operDeCl}
+                  <br/><br/>
+
+                  [야영장 안내]
+                  <br/>
+                  일반야영장: {Campsite.gnrlSiteCo} <br/>
+                  자동차야영장: {Campsite.autoSiteCo} <br/>
+                  글램핑: {Campsite.glampSiteCo} (내부시설: {Campsite.glampInnerFclty}) <br/>
+                  카라반: {Campsite.caravSiteCo} (내부시설: {Campsite.caravInnerFclty}) <br/>
+                  개인카라반: {Campsite.indvdlCaravSiteCo} <br/>
+                  파쇄석: {Campsite.siteBottomCl2} <br/>
+                  잔디: {Campsite.siteBottomCl1} <br/>
+                  테크: {Campsite.siteBottomCl3} <br/>
+                  자갈: {Campsite.siteBottomCl4} <br/>
+                  맨흙: {Campsite.siteBottomCl5}
+                  <br/><br/>
+
+                  [시설 안내]
+                  <br/>
+                  화장실: {Campsite.toiletCo}개 <br/>
+                  샤워실: {Campsite.swrmCo}개 <br/>
+                  개수대: {Campsite.wtrplCo}개 <br/>
+                  화로대: {Campsite.brazierCl} <br/>
+                  부대시설: {Campsite.sbrsCl} <br/>
+                  기타: {Campsite.sbrsEtc}
+                  <br/><br/>
+
+                  [기타 시설 안내]
+                  <br/>
+                  주변 이용 가능 시설: {Campsite.posblFcltyCl} <br/>
+                  {Campsite.posblFcltyEtc   // 기타 주변 이용 가능 시설 여부
+                      ? (
+                          <>
+                            주변 이용 가능 시설 기타: {Campsite.posblFcltyEtc} <br/>
+                          </>
+                      )
+                      :
+                        ""
+                  }
+                  {Campsite.clturEventAt !== "N"    // 자체 문화 행사 여부
+                      ? (
+                          <>
+                            자체 문화 행사명: {Campsite.clturEvent} <br/>
+                          </>
+                      )
+                      : (
+                          <>
+                            자체 문화 행사 없음 <br/>
+                          </>
+                      )
+                  }
+                  {Campsite.exprnProgrmAt !== "N"   // 체험 프로그램 여부
+                      ? (
+                          <>
+                            체험 프로그램명: {Campsite.exprnProgrm} <br/>
+                          </>
+                      )
+                      : (
+                          <>
+                            체험 프로그램 없음 <br/>
+                          </>
+                      )}
+                  <br/><br/>
+
+                  [기타]
+                  <br/>
+                  개인 트레일러: 동반 {Campsite.trlerAcmpnyAt === "Y" ? "가능" : "불가능"} <br/>
+                  개인 카라반: 동반 {Campsite.caravAcmpnyAt === "Y" ? "가능" : "불가능"} <br/>
+                  캠핑 장비 대여: {Campsite.eqpmnLendCl} <br/>
+                  애완 동물 출입: {Campsite.animalCmgCl}
+                  <br/><br/>
+
+                  [예약 안내]
+                  <br/>
+                  {Campsite.resveCl}
+                  <br/>
+                  휴뮤: {Campsite.hvofBgnde} ~ {Campsite.hvofEnddle}
+                  <br/>
+                  <StyledAtag
+                      href={Campsite.resveUrl}
+                      color="#3F4254"
+                      hover="black"
+                  >
+                    ☞ 캠핑장 예약페이지 바로가기
+                  </StyledAtag>
+                  <br/><br/>
+                </GrayDetail>
+              </DetailWrapper>
+            </RightWrapper>
+          </TopWrapper>
 
           {/* 리뷰 */}
           <BottomTitle>Review</BottomTitle>
+
           <BottomWrapper>
 
             {/* 리뷰 목록 */}
@@ -104,7 +275,7 @@ const ContentDetail = () => {
                     <option>1</option>
                   </select>
                 </StarWrapper>
-                <ImgWrapper>
+                <UploadImgWrapper>
                   {/* 이미지 선택 */}
                   <ImgInputLabel htmlFor="img-input">
                     <BiImageAdd />
@@ -121,7 +292,7 @@ const ContentDetail = () => {
                       return <img src={url} key={index}/>
                     })}
                   </PreviewWrapper>
-                </ImgWrapper>
+                </UploadImgWrapper>
                 {/* 내용 입력 */}
                 <TextAreaWrapper>
                   <textarea
--- src/pages/ContentDetail/style.js
+++ src/pages/ContentDetail/style.js
@@ -3,9 +3,93 @@ import StyledBtn from "../../styles/StyledBtn";
 import Color from "../../styles/Color";
 
 export const TopWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+
   width: 80%;
+  margin: 20px 0;
 `
 
+export const LeftWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  //width: 45%;
+  height: fit-content;
+  padding: 0 10px 10px;
+  border: 3px solid ${Color.pointcolor};
+  border-radius: 10px;
+`
+
+export const Title = styled.p`
+  font-size: x-large;
+  font-weight: bold;
+`
+
+export const LeftInfo = styled.span`
+  font-style: italic;
+  display: inline-block;
+  margin: 3px 0;
+`
+
+export const Map = styled.div`
+  width: 100%;
+  margin: 10px 0;
+  border: 1px solid;  // 임시
+`
+
+export const RightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  width: 70%;
+  padding: 0 20px;
+`
+
+export const ImgsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 10px 0;
+  
+  & > img {
+    width: 80%;
+    height: 80%;
+    object-fit: contain;
+  }
+`
+
+export const DetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const ShortComment = styled.div`
+  margin: 10px;
+  font-size: large;
+  
+  & > button {  // 더보기 버튼
+    margin: 0 5px;
+    color: gray;
+    :hover {
+      color: black;
+    }
+  }
+`
+
+export const LongComment = styled.div`
+  display: ${props => props.show ? "block" : "none"};
+  margin: 10px;
+  font-style: italic;
+`
+
+export const GrayDetail = styled.div`
+  color: #3F4254;
+  margin: 10px;
+`
+
+// ====================== Review ======================== //
 export const BottomTitle = styled.p`
   width: 80%;
   border-bottom: 1px solid rgba(0, 0, 0, 0.5);
@@ -84,7 +168,7 @@ export const StarWrapper = styled.div`
   }
 `
 
-export const ImgWrapper = styled.div`
+export const UploadImgWrapper = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
