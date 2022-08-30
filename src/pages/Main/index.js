61f17680db5c5b938e7caee5f7cd634fa684378f
hyowonii <hyowon9964@gmail.com>
2022-08-31 03:46:40 +0900

    Feat/Design: 메인페이지 구현

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from "react-router-dom";
 import Header from "../../components/Header";
+import TourPicsAPI from "../../components/API/TourPicsAPI";
+import {BgImg, Btn, BtnWrapper, ImgSlideWrapper, PhoneImg, Title} from "./style";
+import Phone from "../../assets/Phone.png";
 
 const Main = () => {
+
+  const navigate = useNavigate();
+
+  const [urlList, setUrlList] = useState([]);
+
+  // 관광사진 목록 불러오기
+  useEffect(() => {
+    TourPicsAPI()
+        .then((res) => {
+          console.log("관광사진 목록", res);
+
+          let list = [];
+          res.map((item) => {
+            list.push(item.children[3].value);
+          })
+          setUrlList(list);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  },[]);
+
+/*  // 이미지 슬라이드
+  const ImgSlide = () => {
+    for(let i=0; i<urlList.length; i++) {
+
+    }
+
+    setTimeout(ImgSlide, 3000);
+
+  };*/
+
   return (
       <div>
-        <Header/>
+        <Header />
+        <ImgSlideWrapper>
+          <BgImg bgImg={urlList[0]} />
+          {/*<ul style={{margin: 0, padding: 0, width: "100%", height: "100%"}}>
+            {urlList.map((url, index) => {
+              return (
+                  <li className="slider" style={{width: "100%", height: "100%"}}>
+                    <BgImg bgImg={url} key={index} />
+                  </li>
+              )
+            })}
+          </ul>*/}
+          <Title>Camping Trade</Title>
+          <BtnWrapper>
+            <Btn onClick={() => navigate('/campsites/all')}>
+              🏕 Campsites 🏕
+            </Btn>
+          </BtnWrapper>
+          <PhoneImg src={Phone} />
+        </ImgSlideWrapper>
+
       </div>
   )
 }
