b0c0edcf3aaeb8978de86e4a5d89cc462daf336b
hyowonii <hyowon9964@gmail.com>
2022-09-07 00:48:43 +0900

    Feat: 유저 예약 정보 받아오기

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,5 +1,23 @@
+import axios from "axios";
+import preURL from "../../preURL";
+import {useCookies} from "react-cookie";
+
 const MyPage = () => {
 
+  const [cookies, setCookie, removeCookie] = useCookies(['appToken']);
+
+  axios
+      .get(preURL + '/member/reservations',{
+        headers: {
+          'Authorization': `Bearer ${cookies.appToken}`
+        }
+      })
+      .then((res) => {
+        console.log('👍유저 예약 정보 받아오기 성공', res);
+      })
+      .catch((err) => {
+        console.log('🧨유저 예약 정보 받아오기 실패', err);
+      })
 }
 
 export default MyPage;
